f70b71c401052ebaf04ff27157faa446f9f750eb
unknown <glodekpiotr@wp.pl>
2019-02-02 17:22:52 +0100

    fixes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,30 @@
 "use strict";
 
 (function () {
-  // event delegation
-  document.body.addEventListener('click', function (e) {
-    var target = e.target,
-        body = document.body;
-
-    if (target.classList.contains('hamburger')) {
-      body.classList.toggle('hamburger--open');
-      body.classList.toggle('nav__menu--open');
-    } else if (target.classList.contains('nav__link') && !target.classList.contains('user-info__email')) {
+  var hamburger = document.querySelector('.hamburger'),
+      links = document.querySelectorAll('.nav__link:not(.user-info__email)'),
+      searchBtn = document.querySelector('.nav__search-btn'),
+      projectCreatorbtn = document.querySelector('.nav__link--is-btn'),
+      closeIconProjectCreator = document.querySelector('.project-creator__close'),
+      body = document.body;
+  hamburger.addEventListener('click', function () {
+    body.classList.toggle('hamburger--open');
+    body.classList.toggle('nav__menu--open');
+  });
+  links.forEach(function (link) {
+    link.addEventListener('click', function () {
       body.classList.remove('hamburger--open');
       body.classList.remove('nav__menu--open');
-    }
-
-    if (target.classList.value === 'nav__link nav__link--flex nav__search-btn') {
-      body.classList.toggle('nav__search-btn--open');
-      document.querySelector('.main-content').classList.toggle('main-content--is-pushed-down');
-    }
+    });
+  });
+  searchBtn.addEventListener('click', function () {
+    body.classList.toggle('nav__search-btn--open');
+    document.querySelector('.main-content').classList.toggle('main-content--push');
+  });
+  projectCreatorbtn.addEventListener('click', function () {
+    body.classList.toggle('project-creator--open');
+  });
+  closeIconProjectCreator.addEventListener('click', function () {
+    body.classList.remove('project-creator--open');
   });
 })();
--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,35 @@
-(function () {
-  // event delegation
-  document.body.addEventListener('click', e => {
-    const target = e.target,
-      body = document.body;
-    if (target.classList.contains('hamburger')) {
-      body.classList.toggle('hamburger--open');
-      body.classList.toggle('nav__menu--open');
-    } else if (target.classList.contains('nav__link') && !target.classList.contains('user-info__email')) {
-      body.classList.remove('hamburger--open');
-      body.classList.remove('nav__menu--open');
-    }
-    if (target.classList.value === 'nav__link nav__link--flex nav__search-btn') {
-      body.classList.toggle('nav__search-btn--open');
-      document.querySelector('.main-content').classList.toggle('main-content--is-pushed-down');
-    }
+(function() {
+ const hamburger = document.querySelector('.hamburger'),
+  links = document.querySelectorAll('.nav__link:not(.user-info__email)'),
+  searchBtn = document.querySelector('.nav__search-btn'),
+  projectCreatorbtn = document.querySelector('.nav__link--is-btn'),
+  closeIconProjectCreator = document.querySelector('.project-creator__close'),
+  body = document.body;
+
+ hamburger.addEventListener('click', () => {
+  body.classList.toggle('hamburger--open');
+  body.classList.toggle('nav__menu--open');
+ });
+
+ links.forEach(link => {
+  link.addEventListener('click', () => {
+   body.classList.remove('hamburger--open');
+   body.classList.remove('nav__menu--open');
   });
+ });
+
+ searchBtn.addEventListener('click', () => {
+  body.classList.toggle('nav__search-btn--open');
+  document
+   .querySelector('.main-content')
+   .classList.toggle('main-content--push');
+ });
+
+ projectCreatorbtn.addEventListener('click', () => {
+  body.classList.toggle('project-creator--open');
+ });
+
+ closeIconProjectCreator.addEventListener('click', () => {
+  body.classList.remove('project-creator--open');
+ });
 })();
